5d91fed68352ab20f6cd2020f8c7c953211fefec
Devin Warrick <dev.warr@gmail.com>
2020-04-24 10:50:17 -0500

    Completes Ring buffer MVP; test passes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,17 +4,39 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.oldest = None
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            # If we're at capacity,
+            # overwrite oldest node
+            # and update self.oldest
+            self.oldest.value = item
+
+            if self.oldest == self.storage.tail:
+                self.oldest = self.storage.head
+            else:
+                self.oldest = self.oldest.next
+        else:
+            # Add node in, increase capacity,
+            # And set self.oldest if needed
+            self.storage.add_to_tail(item)
+            if len(self.storage) == 1:
+                # If our capacity is 1, we just added
+                # our first value, and that value
+                # is the oldest
+                self.oldest = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        curr_node = self.storage.head
+        while curr_node is not None:
+            list_buffer_contents.append(curr_node.value)
+            curr_node = curr_node.next
+
 
         return list_buffer_contents
 
